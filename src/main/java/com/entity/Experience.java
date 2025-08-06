675fbc90a40e86b5cc25629940d99b7351947e51
lukovic11 <115374310+Lukovic11@users.noreply.github.com>
2025-08-06 22:40:51 +0200

    basic entity structure

---
+++ src/main/java/com/entity/Experience.java
@@ -0,0 +1,34 @@
+package com.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "experience")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Experience {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "date")
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
---
+++ src/main/java/com/entity/Trip.java
@@ -0,0 +1,40 @@
+package com.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "trip")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "date_from")
+    private Date dateFrom;
+
+    @Column(name = "date_to")
+    private Date dateTo;
+
+    @Column(name = "location")
+    private String location;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
